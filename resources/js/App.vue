b6d32526e493333e3679e65a3226f4378673df91
Keval-sprybit <keval@sprybit.com>
2023-03-09 17:36:03 +0530

    initlize

---
+++ resources/js/App.vue
@@ -0,0 +1,24 @@
+<!-- <template>
+	<div id="app">
+
+		<employee />
+		
+
+	</div>
+</template> -->
+<!-- <template>
+	<div id="app">
+		<hello/>
+	</div>
+</template> -->
+<script>
+	import employee from './components/employee.vue';
+	import hello from './components/HelloWorld.vue';
+
+	export default{
+		components:{
+			employee,
+			hello
+		}
+	}
+</script>
